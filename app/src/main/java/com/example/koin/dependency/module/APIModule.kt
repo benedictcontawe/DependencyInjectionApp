f0ca78718884611d203e81d20b39f734dce975b2
Benedict <benedictcontawe@gmail.com>
2020-03-29 22:17:15 +0800

    Modify Main Activity, Gradle, and API Module

--- app/src/main/java/com/example/koin/dependency/module/APIModule.kt
+++ app/src/main/java/com/example/koin/dependency/module/APIModule.kt
@@ -1,7 +1,12 @@
 package com.example.koin.dependency.module
 
+import com.example.koin.BuildConfig
+import com.example.koin.model.web.CountryAPI
+import com.example.koin.model.web.NetworkClient
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * For API (Retrofit)
@@ -10,12 +15,38 @@ object APIModule {
 
     val apiModule = module {
 
-        //fun provideApi(retrofit: Retrofit) = retrofit.create(GithubRepoApi::class.java)
-
+        single {
+            provideRetrofit()
+        }
         /*
         single {
-            provideApi(get())
+            provideOkHttpClient(get<OkHttpClient>())
         }
         */
+        factory<CountryAPI> {
+            provideCountryApi(get<Retrofit>())
+        }
+
     }
+
+    private fun provideRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_DOMAIN)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    /*
+    private fun provideOkHttpClient(okHttpClient : OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_DOMAIN)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor) : OkHttpClient {
+        return OkHttpClient().newBuilder().addInterceptor(authInterceptor).build()
+    }
+    */
+    private fun provideCountryApi(retrofit : Retrofit) : CountryAPI = retrofit.create(CountryAPI::class.java)
+
 }
--- app/src/main/java/com/example/koin/view/MainActivity.kt
+++ app/src/main/java/com/example/koin/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.koin.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.koin.MainAndroidViewModel
 import com.example.koin.MainViewModel
 import com.example.koin.R
+import com.example.koin.model.web.CountryResponseModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -13,21 +15,18 @@ class MainActivity : AppCompatActivity() {
 
     // Lazy injected MainViewModel
     val mainViewModel : MainViewModel by viewModel<MainViewModel>()
-    val mainAndroidViewModel : MainAndroidViewModel by viewModel<MainAndroidViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainViewModel.sayHello().observe(this, object : Observer<String> {
-            override fun onChanged(data : String?) {
-                text_view_result_view_model.setText("${data}")
-            }
-        })
-
-        mainAndroidViewModel.sayHello().observe(this, object : Observer<String> {
-            override fun onChanged(data : String?) {
-                text_view_result_android_view_model.setText("${data}")
+        mainViewModel.requestCountry().observe(this, object : Observer<List<CountryResponseModel>> {
+            override fun onChanged(list : List<CountryResponseModel>) {
+                text_view_result.text = list
+                    .map {
+                        Log.d("MainActivity",it.Name?:"")
+                        it.Name
+                    }.toString()
             }
         })
     }
