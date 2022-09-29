8042839c0a63e93347a84ed769c241357e9998f6
Benedict Contawe <benedictcontawe@gmail.com>
2022-09-29 21:22:12 +0800

    Update Change App Delegate extension from Application to MultiDexApplication

--- app/src/main/java/com/example/dependencyinjectionapp/AppDelegate.kt
+++ app/src/main/java/com/example/dependencyinjectionapp/AppDelegate.kt
@@ -1,15 +1,19 @@
 package com.example.dependencyinjectionapp
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 /**
  * An application with @HiltAndroidApp that triggers Hilt's code generation and
  * adds an application-level dependency container.
  */
 @HiltAndroidApp
-class AppDelegate : Application() {
+class AppDelegate : MultiDexApplication() {
 
-    override fun onCreate() {
-        super.onCreate()
+    companion object {
+        private val TAG : String = AppDelegate::class.java.getSimpleName()
+    }
+
+    init {
+
     }
 }
